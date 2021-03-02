9288f9b56991a802d12ee30d4f20c81a286a06fb
jeroen_pc_home <jeroen_kruyt@hotmail.com>
2021-03-02 13:37:25 +0100

    fun times breaking everthing
    world and charater has fall mechinacis

---
+++ Assets/Scenes/MovementController.cs
@@ -0,0 +1,21 @@
+
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    public float MovementSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+    }
+}
