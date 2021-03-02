d3d987771ec8c503638268b9f782d1a32d1fd7d9
jeroen_pc_home <jeroen_kruyt@hotmail.com>
2021-03-02 14:20:27 +0100

    i fixed my dumass mistake
    floor stays where it suppossed to be and not falling \

--- Assets/Scenes/MovementController.cs
+++ Assets/Scenes/MovementController.cs
@@ -5,11 +5,13 @@ public class MovementController : MonoBehaviour
 {
 
     public float MovementSpeed = 1;
+    public float JumpForce = 1;
 
+    private Rigidbody _rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,5 +19,10 @@ public class MovementController : MonoBehaviour
     {
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        if(Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y)< 0.001f)
+        {
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
     }
 }
